15a7ad2c420b09cddc97ac6cb785f40399e20bbf
Araj Beach Club <araj.chia@tiscali.it>
2026-06-11 09:34:33 +0200

    Update gestionale.py

--- gestionale.py
+++ gestionale.py
@@ -121,8 +121,8 @@ CAPIENZA_FILE = {
     "Prima Fila": 17,
     "Seconda Fila": 16,
     "Terza Fila": 9,
-    "Quarta Fila": 8,
-    "Quinta Fila": 8,
+    "Quarta Fila": 7,
+    "Quinta Fila": 5,
     "Sesta Fila (Altre)": 8,
     "Spiaggia Libera / Esterna": 5
 }
